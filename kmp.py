35a6135304877a34a32ea38a983b22d402791615
miguelllamas <34238731+miguelllamas@users.noreply.github.com>
2020-09-09 19:32:43 +0800

    Revert "minor nitpick tweaks, fix misspelling"
    This reverts commit e5586cbf431596cffb6fd18ffa7ad9969b250ff5.

--- kmp.py
+++ kmp.py
@@ -7,63 +7,63 @@ import csv
 import datetime
 import psutil
 
-# Python program for KMP algorithm
-def KMPSearch(pat, txt):
-    M = len(pat)
-    N = len(txt)
-
-    # create lps[] that will hold the longest prefix suffix
-    # values for pattern
-    lps = [0]*M
-    j = 0 # index for pat[]
+# Python program for KMP Algorithm 
+def KMPSearch(pat, txt): 
+    M = len(pat) 
+    N = len(txt) 
   
-    # preprocess the pattern (calculate lps[] array)
-    computeLPSArray(pat, M, lps)
+    # create lps[] that will hold the longest prefix suffix  
+    # values for pattern 
+    lps = [0]*M 
+    j = 0 # index for pat[] 
   
-    i = 0 # index for txt[]
-    while i < N:
-        if pat[j] == txt[i]:
+    # Preprocess the pattern (calculate lps[] array) 
+    computeLPSArray(pat, M, lps) 
+  
+    i = 0 # index for txt[] 
+    while i < N: 
+        if pat[j] == txt[i]: 
             i += 1
             j += 1
   
-        if j == M:
+        if j == M: 
             # print("Found pattern at index " + str(i-j))
             # print("MATCH: " + pat)
-            j = lps[j-1]
+            j = lps[j-1] 
             return True
             # break
   
-        # mismatch after j matches
-        elif i < N and pat[j] != txt[i]:
-            # Do not match lps[0..lps[j-1]] characters,
-            # they will match anyway
-            if j != 0:
-                j = lps[j-1]
-            else:
+        # mismatch after j matches 
+        elif i < N and pat[j] != txt[i]: 
+            # Do not match lps[0..lps[j-1]] characters, 
+            # they will match anyway 
+            if j != 0: 
+                j = lps[j-1] 
+            else: 
                 i += 1
     return False
   
-def computeLPSArray(pat, M, lps):
-    len = 0 # length of the previous longest prefix suffix
+def computeLPSArray(pat, M, lps): 
+    len = 0 # length of the previous longest prefix suffix 
   
-    lps[0] # lps[0] is always 0
+    lps[0] # lps[0] is always 0 
     i = 1
-
-    # the loop calculates lps[i] for i = 1 to M-1
-    while i < M:
-        if pat[i] == pat[len]:
+  
+    # the loop calculates lps[i] for i = 1 to M-1 
+    while i < M: 
+        if pat[i]== pat[len]: 
             len += 1
             lps[i] = len
             i += 1
-        else:
-            # This is tricky. Consider the example.
-            # AAACAAAA and i = 7. The idea is similar
-            # to search step.
-            if len != 0:
-                len = lps[len-1]
+        else: 
+            # This is tricky. Consider the example. 
+            # AAACAAAA and i = 7. The idea is similar  
+            # to search step. 
+            if len != 0: 
+                len = lps[len-1] 
   
-                # Also, note that we do not increment i here
-            else:
+                # Also, note that we do not increment i here 
+            else: 
                 lps[i] = 0
                 i += 1
 
@@ -80,7 +80,7 @@ def fileCheck(fname):
     errors=[]
 
     # Compare every word in sample to every word in dictionary via pattern match
-    print("Misspelled words:")
+    print("Mispelled words:")
     start = time.time()
     for word in words:
         found=False
@@ -88,16 +88,16 @@ def fileCheck(fname):
             if not found and len(d_word) == len(word):
                 found = KMPSearch(word, d_word) 
 
-        # Print out misspelled words
+        # print out mispelled words
         if not found:
             errors.append(word)
             print(word)
 
     end = time.time()
-    process = psutil.Process(os.getpid())
+    process = psutil.Process(os.getpid()) 
     memoryUse = process.memory_info().rss
-    cpu = psutil.cpu_percent()
-    ram = psutil.virtual_memory().percent
+    cpu=psutil.cpu_percent()
+    ram=psutil.virtual_memory().percent
     return [len(words),len(errors),(end-start),memoryUse,ram,cpu]
 
 def writeResults(row):
